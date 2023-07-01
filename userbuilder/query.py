f08c264b9f7bee3c2034dda98dbc7331441010d9
Amir <amirhasantalebi1400@gmail.com>
2023-06-30 21:45:21 -0700

    Create database and optimaze it

---
+++ userbuilder/query.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+# connect database
+conn = mysql.connector.connect(host = '127.0.0.1',
+                   user = 'root',
+                   passwd = 'Amir10$$',
+                   database = 'user')
+
+# print(conn)
+
+# Space
+mycursor = conn.cursor()
+
+# create database
+# mycursor.execute("CREATE DATABASE user")
+
+# create table
+# mycursor.execute("CREATE TABLE person(id VARCHAR(255),name VARCHAR(255),family VARCHAR(255))")
+
