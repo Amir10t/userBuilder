78625a55b80f4f937c95906b0fe77bbcd4659a22
Amir <amirhasantalebi1400@gmail.com>
2023-07-05 04:41:32 -0700

    add a query to query.py file

--- userbuilder/query.py
+++ userbuilder/query.py
@@ -16,3 +16,15 @@ mycursor = conn.cursor()
 
 # create table
 # mycursor.execute("CREATE TABLE person(id VARCHAR(255),name VARCHAR(255),family VARCHAR(255))")
+
+#for delete data
+# mycursor.execute("DELETE FROM person WHERE id=''")
+# conn.commit()
+
+# sent back datas
+mycursor.execute("SELECT * FROM person")
+result = mycursor.fetchall()
+
+for x in result:
+    print(x)
+    
